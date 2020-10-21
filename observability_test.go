666286d8db3486f4141bce762dc0fd3f210409d1
Song Song Li <ssli@redhat.com>
2020-10-21 10:02:52 +0800

    check operator and required crds

--- observability_test.go
+++ observability_test.go
@@ -1,35 +1,49 @@
 package main_test
 
 import (
-	"fmt"
-	"io"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/open-cluster-management/observability-e2e-test/utils"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+
+	"github.com/open-cluster-management/observability-e2e-test/utils"
 )
 
 const (
-	OBSERVABILITY_POD_NAMESPACE = "open-cluster-management"
-	OBSERVABILITY_DEPLOY_PREFIX = "multicluster-observability-operator"
-	OBSERVABILITY_LABEL         = "name=multicluster-observability-operator"
+	MCO_OPERATOR_NAMESPACE = "open-cluster-management"
+	MCO_POD_NAMESPACE      = "open-cluster-management-observability"
+	MCO_LABEL              = "name=multicluster-observability-operator"
 )
 
-var _ = Describe("MCO Operator testing", func() {
+var (
+	EventuallyTimeoutMinute  time.Duration = 60 * time.Second
+	EventuallyIntervalSecond time.Duration = 1 * time.Second
+)
+
+var _ = Describe("testing all observability features", func() {
 	var hubClient kubernetes.Interface
 	BeforeEach(func() {
-		io.WriteString(GinkgoWriter, fmt.Sprintf("\n\nConnecting to the Hub with master-url: %s\n\tcontext: %s\n\tfrom kubeconfig: %s\n\n", testOptions.HubCluster.MasterURL, testOptions.HubCluster.KubeContext, testOptions.KubeConfig))
+		By("connecting to the hub cluster")
 		hubClient = utils.NewKubeClient(testOptions.HubCluster.MasterURL, testOptions.KubeConfig, testOptions.HubCluster.KubeContext)
 	})
 
-	It("should have the expected running mco operator in namespace: open-cluster-management (ingress/g0)", func() {
-		var podList, _ = hubClient.CoreV1().Pods(OBSERVABILITY_POD_NAMESPACE).List(metav1.ListOptions{LabelSelector: OBSERVABILITY_LABEL})
+	It("should have the expected running mco operator in namespace: open-cluster-management", func() {
+		var podList, _ = hubClient.CoreV1().Pods(MCO_OPERATOR_NAMESPACE).List(metav1.ListOptions{LabelSelector: MCO_LABEL})
 		Expect(len(podList.Items)).To(Equal(1))
 		for _, pod := range podList.Items {
 			Expect(string(pod.Status.Phase)).To(Equal("Running"))
 		}
+
+		By("checking required CRDs have existed")
+		Eventually(func() error {
+			return utils.HaveCRDs(testOptions.HubCluster, testOptions.KubeConfig,
+				[]string{
+					"multiclusterobservabilities.observability.open-cluster-management.io",
+					"observatoria.core.observatorium.io",
+					"observabilityaddons.observability.open-cluster-management.io",
+				})
+		}).Should(Succeed())
 	})
 })
