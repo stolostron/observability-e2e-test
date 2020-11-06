c6ddd6d318a139e85ceda080b647979e7ccc096d
Song Song Li <ssli@redhat.com>
2020-11-06 14:36:28 +0800

    add env to skip install step

--- observability-e2e-test_suite_test.go
+++ observability-e2e-test_suite_test.go
@@ -93,53 +93,54 @@ var agoutiDriver *agouti.WebDriver
 var _ = BeforeSuite(func() {
 	initVars()
 
-	hubClient := utils.NewKubeClient(
-		testOptions.HubCluster.MasterURL,
-		testOptions.KubeConfig,
-		testOptions.HubCluster.KubeContext)
+	if os.Getenv("SKIP_INSTALL_STEP") != "true" {
+		hubClient := utils.NewKubeClient(
+			testOptions.HubCluster.MasterURL,
+			testOptions.KubeConfig,
+			testOptions.HubCluster.KubeContext)
 
-	dynClient := utils.NewKubeClientDynamic(
-		testOptions.HubCluster.MasterURL,
-		testOptions.KubeConfig,
-		testOptions.HubCluster.KubeContext)
+		dynClient := utils.NewKubeClientDynamic(
+			testOptions.HubCluster.MasterURL,
+			testOptions.KubeConfig,
+			testOptions.HubCluster.KubeContext)
 
-	EventuallyTimeoutMinute := 60 * time.Second
-	EventuallyIntervalSecond := 1 * time.Second
+		EventuallyTimeoutMinute := 60 * time.Second
+		EventuallyIntervalSecond := 1 * time.Second
 
-	By("Checking MCO operator is existed")
-	var podList, _ = hubClient.CoreV1().Pods(MCO_OPERATOR_NAMESPACE).List(metav1.ListOptions{LabelSelector: MCO_LABEL})
-	Expect(len(podList.Items)).To(Equal(1))
-	for _, pod := range podList.Items {
-		Expect(string(pod.Status.Phase)).To(Equal("Running"))
-	}
-
-	By("Checking Required CRDs is existed")
-	Eventually(func() error {
-		return utils.HaveCRDs(testOptions.HubCluster, testOptions.KubeConfig,
-			[]string{
-				"multiclusterobservabilities.observability.open-cluster-management.io",
-				"observatoria.core.observatorium.io",
-				"observabilityaddons.observability.open-cluster-management.io",
-			})
-	}).Should(Succeed())
-
-	Expect(utils.CreateMCONamespace(testOptions)).NotTo(HaveOccurred())
-	Expect(utils.CreatePullSecret(testOptions)).NotTo(HaveOccurred())
-	Expect(utils.CreateObjSecret(testOptions)).NotTo(HaveOccurred())
-
-	By("Creating MCO instance")
-	mco := utils.NewMCOInstanceYaml(MCO_CR_NAME)
-	Expect(utils.Apply(testOptions.HubCluster.MasterURL, testOptions.KubeConfig, testOptions.HubCluster.KubeContext, mco)).NotTo(HaveOccurred())
-
-	By("Waiting for MCO ready status")
-	Eventually(func() bool {
-		instance, err := dynClient.Resource(utils.NewMCOGVR()).Get(MCO_CR_NAME, metav1.GetOptions{})
-		if err == nil {
-			return utils.StatusContainsTypeEqualTo(instance, "Ready")
+		By("Checking MCO operator is existed")
+		var podList, _ = hubClient.CoreV1().Pods(MCO_OPERATOR_NAMESPACE).List(metav1.ListOptions{LabelSelector: MCO_LABEL})
+		Expect(len(podList.Items)).To(Equal(1))
+		for _, pod := range podList.Items {
+			Expect(string(pod.Status.Phase)).To(Equal("Running"))
 		}
-		return false
-	}, EventuallyTimeoutMinute*5, EventuallyIntervalSecond*5).Should(BeTrue())
 
+		By("Checking Required CRDs is existed")
+		Eventually(func() error {
+			return utils.HaveCRDs(testOptions.HubCluster, testOptions.KubeConfig,
+				[]string{
+					"multiclusterobservabilities.observability.open-cluster-management.io",
+					"observatoria.core.observatorium.io",
+					"observabilityaddons.observability.open-cluster-management.io",
+				})
+		}).Should(Succeed())
+
+		Expect(utils.CreateMCONamespace(testOptions)).NotTo(HaveOccurred())
+		Expect(utils.CreatePullSecret(testOptions)).NotTo(HaveOccurred())
+		Expect(utils.CreateObjSecret(testOptions)).NotTo(HaveOccurred())
+
+		By("Creating MCO instance")
+		mco := utils.NewMCOInstanceYaml(MCO_CR_NAME)
+		Expect(utils.Apply(testOptions.HubCluster.MasterURL, testOptions.KubeConfig, testOptions.HubCluster.KubeContext, mco)).NotTo(HaveOccurred())
+
+		By("Waiting for MCO ready status")
+		Eventually(func() bool {
+			instance, err := dynClient.Resource(utils.NewMCOGVR()).Get(MCO_CR_NAME, metav1.GetOptions{})
+			if err == nil {
+				return utils.StatusContainsTypeEqualTo(instance, "Ready")
+			}
+			return false
+		}, EventuallyTimeoutMinute*5, EventuallyIntervalSecond*5).Should(BeTrue())
+	}
 })
 
 var _ = AfterSuite(func() {
