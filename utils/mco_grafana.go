4f041d637888011bc9a831a2ee24df98639eb683
clyang82 <chuyang@redhat.com>
2020-11-09 13:34:59 +0800

    Enable test in KinD cluster

--- utils/mco_grafana.go
+++ utils/mco_grafana.go
@@ -6,8 +6,19 @@ import (
 	"net/http"
 )
 
+func GetGrafanaURL(opt TestOptions) string {
+	grafanaConsoleURL := "https://multicloud-console.apps." + opt.HubCluster.BaseDomain + "/grafana/"
+	if opt.HubCluster.GrafanaURL != "" {
+		grafanaConsoleURL = opt.HubCluster.GrafanaURL
+	} else {
+		opt.HubCluster.GrafanaHost = "multicloud-console.apps." + opt.HubCluster.BaseDomain
+	}
+	return grafanaConsoleURL
+}
+
 func CheckGrafanaConsole(opt TestOptions) error {
-	req, err := http.NewRequest("GET", "https://multicloud-console.apps."+opt.HubCluster.BaseDomain+"/grafana/", nil)
+	grafanaConsoleURL := GetGrafanaURL(opt)
+	req, err := http.NewRequest("GET", grafanaConsoleURL, nil)
 	if err != nil {
 		return err
 	}
@@ -20,8 +31,11 @@ func CheckGrafanaConsole(opt TestOptions) error {
 	if err != nil {
 		return err
 	}
+	if token != "" {
+		req.Header.Set("Authorization", "Bearer "+token)
+	}
+	req.Host = opt.HubCluster.GrafanaHost
 
-	req.Header.Set("Authorization", "Bearer "+token)
 	resp, err := client.Do(req)
 	if err != nil {
 		return err
