f5276101fda1bda4150ed3645a91a2db16d9d33b
Marco <llan@redhat.com>
2020-11-25 16:19:17 -0600

    add test for custom dashboards

---
+++ utils/mco_dashboard.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"k8s.io/klog"
+)
+
+func ContainDashboard(opt TestOptions, title string) (error, bool) {
+	grafanaConsoleURL := GetGrafanaURL(opt)
+	path := "/api/search?query="
+	queryParams := url.PathEscape(fmt.Sprintf("query=%s", title))
+	req, err := http.NewRequest(
+		"GET",
+		grafanaConsoleURL+path+queryParams,
+		nil)
+	if err != nil {
+		return err, false
+	}
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	client := &http.Client{Transport: tr}
+	token, err := FetchBearerToken(opt)
+	if err != nil {
+		return err, false
+	}
+	if token != "" {
+		req.Header.Set("Authorization", "Bearer "+token)
+	}
+	req.Host = opt.HubCluster.GrafanaHost
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err, false
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		klog.Errorf("resp.StatusCode: %v\n", resp.StatusCode)
+		return fmt.Errorf("Failed to access grafana api"), false
+	}
+
+	result, err := ioutil.ReadAll(resp.Body)
+	klog.V(1).Infof("result: %s\n", result)
+	if err != nil {
+		return err, false
+	}
+
+	if !strings.Contains(string(result), fmt.Sprintf(`"title":"%s"`, title)) {
+		return fmt.Errorf("Failed to find the dashboard"), false
+	} else {
+		return nil, true
+	}
+}
