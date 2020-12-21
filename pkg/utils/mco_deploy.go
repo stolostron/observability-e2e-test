dc99142cfc6566c1b98bf7392afc7aaecccc64a1
clyang82 <chuyang@redhat.com>
2020-12-21 09:35:40 +0800

    update ignore implementation

--- pkg/utils/mco_deploy.go
+++ pkg/utils/mco_deploy.go
@@ -4,6 +4,7 @@ import (
 	b64 "encoding/base64"
 	"fmt"
 	"os"
+	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -192,19 +193,11 @@ func CheckAllPodNodeSelector(opt TestOptions) error {
 	//shard-1-0 and shard-2-0 won't be deleted when switch from High to Basic
 	//And cannot apply the nodeSelector to shard-1-0 and shard-2-0
 	//https://github.com/open-cluster-management/backlog/issues/6532
-	ignorePodList := []string{MCO_CR_NAME + "-observatorium-thanos-store-shard-1-0", MCO_CR_NAME + "-observatorium-thanos-store-shard-2-0"}
+	ignorePods := MCO_CR_NAME + "-observatorium-thanos-store-shard-1-0," + MCO_CR_NAME + "-observatorium-thanos-store-shard-2-0"
 
 	for _, pod := range podList {
-		isIgnorePod := false
-		for _, ignorePod := range ignorePodList {
-			if ignorePod == pod.GetName() {
-				isIgnorePod = true
-				break
-			}
-		}
-
-		if isIgnorePod {
-			continue
+		if strings.Contains(ignorePods, pod.GetName()) {
+			break
 		}
 
 		selecterValue, ok := pod.Spec.NodeSelector["kubernetes.io/os"]
