ea1bc7cfadd82afdb28af4b5a1dda2807d67ecec
clyang82 <chuyang@redhat.com>
2021-01-05 21:11:20 +0800

    Add limitRange and ResourceQuota

--- pkg/tests/observability_install_test.go
+++ pkg/tests/observability_install_test.go
@@ -7,6 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/open-cluster-management/observability-e2e-test/pkg/kustomize"
 	"github.com/open-cluster-management/observability-e2e-test/pkg/utils"
 )
 
@@ -46,6 +47,10 @@ func installMCO() {
 	Expect(utils.CreateMCONamespace(testOptions)).NotTo(HaveOccurred())
 	Expect(utils.CreatePullSecret(testOptions)).NotTo(HaveOccurred())
 	Expect(utils.CreateObjSecret(testOptions)).NotTo(HaveOccurred())
+	//set resource quota and limit range for canary environment to avoid destruct the node
+	yamlB, err := kustomize.Render(kustomize.Options{KustomizationPath: "../../observability-gitops/policy"})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(utils.Apply(testOptions.HubCluster.MasterURL, testOptions.KubeConfig, testOptions.HubCluster.KubeContext, yamlB)).NotTo(HaveOccurred())
 
 	By("Creating MCO instance")
 	mco := utils.NewMCOInstanceYaml(MCO_CR_NAME)
