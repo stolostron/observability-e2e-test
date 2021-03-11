bbbb0cb68f98f713adbc9f2ce29954857560898a
morvencao <morvencao@gmail.com>
2021-03-11 14:54:46 +0800

    fix path issue.

--- cicd-scripts/run-e2e-tests.sh
+++ cicd-scripts/run-e2e-tests.sh
@@ -1,19 +1,17 @@
 # Copyright (c) 2021 Red Hat, Inc.
 # Copyright Contributors to the Open Cluster Management project
 
-WORKDIR=`pwd`
-cd ${WORKDIR}/..
-./cicd-scripts/e2e-tests-setup.sh -a install
+ROOTDIR="$(cd "$(dirname "$0")/.." ; pwd -P)"
+${ROOTDIR}/cicd-scripts/e2e-tests-setup.sh -a install
 if [ $? -ne 0 ]; then
     echo "Cannot setup environment successfully."
     exit 1
 fi
 
 # run test cases
-cd ${WORKDIR}
-./cicd-scripts/tests.sh
+${ROOTDIR}/cicd-scripts/tests.sh
 if [ $? -ne 0 ]; then
     echo "Cannot pass all test cases."
-    cat ${WORKDIR}/pkg/tests/results.xml
+    cat ${ROOTDIR}/pkg/tests/results.xml
     exit 1
 fi
