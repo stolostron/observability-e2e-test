56a42d54e207af399a04bd5e162b30cf6b2648a6
clyang82 <chuyang@redhat.com>
2021-03-24 22:15:38 +0800

    Disable some tests temporarily
    Signed-off-by: clyang82 <chuyang@redhat.com>

--- pkg/tests/observability_reconcile_test.go
+++ pkg/tests/observability_reconcile_test.go
@@ -4,12 +4,10 @@
 package tests
 
 import (
-	"fmt"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 
@@ -54,34 +52,24 @@ var _ = Describe("Observability:", func() {
 		Expect(err).ToNot(HaveOccurred())
 	})
 
-	It("[P1][Sev1][Observability] Checking MCO components in Basic mode (reconcile/g0)", func() {
-		By("Checking MCO components in Basic mode")
-		Eventually(func() error {
-			err = utils.CheckMCOComponentsInBaiscMode(testOptions)
-			if err != nil {
-				return err
-			}
-			return nil
-		}, EventuallyTimeoutMinute*5, EventuallyIntervalSecond*5).Should(Succeed())
-	})
-
-	It("[P1][Sev1][Observability] Modifying retentionResolutionRaw (reconcile/g0)", func() {
-		By("Waiting for MCO retentionResolutionRaw filed to take effect")
-		Eventually(func() error {
-			name := MCO_CR_NAME + "-thanos-compact"
-			compact, getError := hubClient.AppsV1().StatefulSets(MCO_NAMESPACE).Get(name, metav1.GetOptions{})
-			if getError != nil {
-				return getError
-			}
-			argList := compact.Spec.Template.Spec.Containers[0].Args
-			for _, arg := range argList {
-				if arg == "--retention.resolution-raw=3d" {
-					return nil
-				}
-			}
-			return fmt.Errorf("Failed to find modified retention field")
-		}, EventuallyTimeoutMinute*5, EventuallyIntervalSecond*5).Should(Succeed())
-	})
+	// TODO: clyang82 add it back after moving to v1beta2
+	// It("[P1][Sev1][Observability] Modifying retentionResolutionRaw (reconcile/g0)", func() {
+	// 	By("Waiting for MCO retentionResolutionRaw filed to take effect")
+	// 	Eventually(func() error {
+	// 		name := MCO_CR_NAME + "-thanos-compact"
+	// 		compact, getError := hubClient.AppsV1().StatefulSets(MCO_NAMESPACE).Get(name, metav1.GetOptions{})
+	// 		if getError != nil {
+	// 			return getError
+	// 		}
+	// 		argList := compact.Spec.Template.Spec.Containers[0].Args
+	// 		for _, arg := range argList {
+	// 			if arg == "--retention.resolution-raw=3d" {
+	// 				return nil
+	// 			}
+	// 		}
+	// 		return fmt.Errorf("Failed to find modified retention field")
+	// 	}, EventuallyTimeoutMinute*5, EventuallyIntervalSecond*5).Should(Succeed())
+	// })
 
 	It("[P1][Sev1][Observability] Checking node selector for all pods (reconcile/g0)", func() {
 		By("Checking node selector for all pods")
@@ -106,14 +94,11 @@ var _ = Describe("Observability:", func() {
 	})
 
 	It("[P1][Sev1][Observability] Revert MCO CR changes (reconcile/g0)", func() {
-		if !utils.IsCanaryEnvironment(testOptions) {
-			Skip("should skip the high basic mode (reconcile/g0)")
-		}
 		By("Revert MCO CR changes")
 		err := utils.RevertMCOCRModification(testOptions)
 		Expect(err).ToNot(HaveOccurred())
 
-		By("Checking MCO components in High mode")
+		By("Checking MCO components in default HA mode")
 		Eventually(func() error {
 			err = utils.CheckMCOComponentsInHighMode(testOptions)
 			if err != nil {
