da8a5a16eca85b5739cff2a06a80b8f985a820fb
Wei Shi <wshi@redhat.com>
2021-03-26 13:06:09 +0800

    Revert RHACM4K-1449 dc32f10 (#135)
    * Revert "disable the test for default allow list temporarily (#132)"
    
    This reverts commit 59e6ba6d2fb554f5624985b1b7b385807ea8dd91.
    
    * Revert "Add test case for RHACM4K-1449 (#120)"
    
    This reverts commit dc32f1017b04a0e64aad3bb3aa85e2c25b36958b.

--- pkg/utils/mco_metric.go
+++ pkg/utils/mco_metric.go
@@ -5,7 +5,6 @@ package utils
 
 import (
 	"crypto/tls"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -14,71 +13,6 @@ import (
 
 	"k8s.io/klog"
 )
-
-func GetPrometheusURL(opt TestOptions) string {
-	prometheusURL := "https://prometheus-k8s-openshift-monitoring.apps." + opt.HubCluster.BaseDomain
-	return prometheusURL
-}
-
-func GetPrometheusMetricsMetadata(opt TestOptions) (error, []string) {
-	prometheusURL := GetPrometheusURL(opt)
-	path := "/api/v1/metadata"
-	klog.V(1).Infof("request url is: %s\n", prometheusURL+path)
-	req, err := http.NewRequest(
-		"GET",
-		prometheusURL+path,
-		nil)
-	if err != nil {
-		return err, nil
-	}
-
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-
-	client := &http.Client{Transport: tr}
-	token, err := FetchBearerToken(opt)
-	if err != nil {
-		return err, nil
-	}
-	if token != "" {
-		req.Header.Set("Authorization", "Bearer "+token)
-	}
-	req.Host = opt.HubCluster.GrafanaHost
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return err, nil
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		klog.Errorf("resp.StatusCode: %v\n", resp.StatusCode)
-		return fmt.Errorf("Failed to access prometheus metrics metadata"), nil
-	}
-
-	metricResult, err := ioutil.ReadAll(resp.Body)
-	klog.V(1).Infof("queryResult: %s\n", metricResult)
-	if err != nil {
-		return err, nil
-	}
-
-	if !strings.Contains(string(metricResult), `"status":"success"`) {
-		return fmt.Errorf("Failed to find valid status from response"), nil
-	}
-
-	metaData := make(map[string]interface{})
-
-	err = json.Unmarshal([]byte(metricResult), &metaData)
-
-	data := metaData["data"].(map[string]interface{})
-
-	names := make([]string, 0, len(data))
-	for k := range data {
-		names = append(names, k)
-	}
-
-	return nil, names
-}
 
 func ContainManagedClusterMetric(opt TestOptions, query string, matchedLabels []string) (error, bool) {
 	grafanaConsoleURL := GetGrafanaURL(opt)
