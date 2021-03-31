98d0f076f647bb89070005356bb49f74f1a25529
clyang82 <chuyang@redhat.com>
2021-03-31 19:49:56 +0800

    fix compact name

--- pkg/tests/observability_reconcile_test.go
+++ pkg/tests/observability_reconcile_test.go
@@ -54,7 +54,7 @@ var _ = Describe("Observability:", func() {
 	It("[P1][Sev1][Observability] Modifying retentionResolutionRaw (reconcile/g0)", func() {
 		By("Waiting for MCO retentionResolutionRaw filed to take effect")
 		Eventually(func() error {
-			name := MCO_CR_NAME + "-thanos-compact"
+			name := MCO_CR_NAME + "-observatorium-thanos-compact"
 			compact, getError := hubClient.AppsV1().StatefulSets(MCO_NAMESPACE).Get(name, metav1.GetOptions{})
 			if getError != nil {
 				return getError
