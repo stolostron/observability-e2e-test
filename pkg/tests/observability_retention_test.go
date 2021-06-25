5bae064a8c1ce595f11078b7414df58df215f366
hchenxa <huichen@redhat.com>
2021-06-25 22:12:44 +0800

    the description did not have the updated value
    Signed-off-by: hchenxa <huichen@redhat.com>

--- pkg/tests/observability_retention_test.go
+++ pkg/tests/observability_retention_test.go
@@ -5,6 +5,7 @@ package tests
 
 import (
 	"fmt"
+	"math"
 	"reflect"
 	"strconv"
 
@@ -21,7 +22,7 @@ var _ = Describe("Observability:", func() {
 		deleteDelay              = "48h"
 		retentionInLocal         = "24h"
 		blockDuration            = "2h"
-		ignoreDeletionMarksDelay = ""
+		ignoreDeletionMarksDelay = "24h"
 	)
 
 	BeforeEach(func() {
@@ -38,12 +39,15 @@ var _ = Describe("Observability:", func() {
 		if err != nil {
 			panic(err.Error())
 		}
+
 		if _, adv := mcoRes.Object["spec"].(map[string]interface{})["advanced"]; adv {
 			if _, rec := mcoRes.Object["spec"].(map[string]interface{})["advanced"].(map[string]interface{})["retentionConfig"]; rec {
 				for k, v := range mcoRes.Object["spec"].(map[string]interface{})["advanced"].(map[string]interface{})["retentionConfig"].(map[string]interface{}) {
 					switch k {
 					case "deleteDelay":
 						deleteDelay = reflect.ValueOf(v).String()
+						idmk, _ := strconv.Atoi(deleteDelay[:len(deleteDelay)-1])
+						ignoreDeletionMarksDelay = fmt.Sprintf("%.f", math.Ceil(float64(idmk)/float64(2))) + deleteDelay[len(deleteDelay)-1:]
 					case "retentionInLocal":
 						retentionInLocal = reflect.ValueOf(v).String()
 					case "blockDuration":
@@ -52,14 +56,10 @@ var _ = Describe("Observability:", func() {
 				}
 			}
 		}
-		idmk, err := strconv.Atoi(deleteDelay[:len(deleteDelay)-1])
-		if err != nil {
-			panic(err.Error())
-		}
-		ignoreDeletionMarksDelay = strconv.Itoa(idmk/2) + deleteDelay[len(deleteDelay)-1:]
 	})
 
-	It("[P2][Sev2][Observability][Stable] Check compact args: --delete-delay="+deleteDelay+" (retention/g0)", func() {
+	It("[P2][Sev2][Observability][Stable] Check compact args:", func() {
+		By("--delete-delay=" + deleteDelay + " (retention/g0)")
 		Eventually(func() error {
 			name := MCO_CR_NAME + "-thanos-compact"
 			compact, err := hubClient.AppsV1().StatefulSets(MCO_NAMESPACE).Get(name, metav1.GetOptions{})
@@ -76,7 +76,8 @@ var _ = Describe("Observability:", func() {
 		}, EventuallyTimeoutMinute*1, EventuallyIntervalSecond*5).Should(Succeed())
 	})
 
-	It("[P2][Sev2][Observability][Stable] Check store args: --ignore-deletion-marks-delay="+ignoreDeletionMarksDelay+" (retention/g0)", func() {
+	It("[P2][Sev2][Observability][Stable] Check store args:", func() {
+		By("--ignore-deletion-marks-delay=" + ignoreDeletionMarksDelay + " (retention/g0)")
 		Eventually(func() error {
 			name := MCO_CR_NAME + "-thanos-store-shard-0"
 			store, err := hubClient.AppsV1().StatefulSets(MCO_NAMESPACE).Get(name, metav1.GetOptions{})
@@ -93,7 +94,8 @@ var _ = Describe("Observability:", func() {
 		}, EventuallyTimeoutMinute*1, EventuallyIntervalSecond*5).Should(Succeed())
 	})
 
-	It("[P2][Sev2][Observability][Stable] Check receive args: --tsdb.retention="+retentionInLocal+" (retention/g0)", func() {
+	It("[P2][Sev2][Observability][Stable] Check receive args:", func() {
+		By("--tsdb.retention=" + retentionInLocal + " (retention/g0)")
 		Eventually(func() error {
 			name := MCO_CR_NAME + "-thanos-receive-default"
 			receive, err := hubClient.AppsV1().StatefulSets(MCO_NAMESPACE).Get(name, metav1.GetOptions{})
@@ -110,7 +112,8 @@ var _ = Describe("Observability:", func() {
 		}, EventuallyTimeoutMinute*1, EventuallyIntervalSecond*5).Should(Succeed())
 	})
 
-	It("[P2][Sev2][Observability][Stable] Check rule args: --tsdb.retention="+retentionInLocal+" (retention/g0)", func() {
+	It("[P2][Sev2][Observability][Stable] Check rule args:", func() {
+		By("--tsdb.retention=" + retentionInLocal + " (retention/g0)")
 		Eventually(func() error {
 			name := MCO_CR_NAME + "-thanos-rule"
 			rule, err := hubClient.AppsV1().StatefulSets(MCO_NAMESPACE).Get(name, metav1.GetOptions{})
@@ -127,7 +130,8 @@ var _ = Describe("Observability:", func() {
 		}, EventuallyTimeoutMinute*1, EventuallyIntervalSecond*5).Should(Succeed())
 	})
 
-	It("[P2][Sev2][Observability][Stable] Check rule args: --tsdb.block-duration="+blockDuration+" (retention/g0)", func() {
+	It("[P2][Sev2][Observability][Stable] Check rule args:", func() {
+		By("--tsdb.block-duration=" + blockDuration + " (retention/g0)")
 		Eventually(func() error {
 			name := MCO_CR_NAME + "-thanos-rule"
 			rule, err := hubClient.AppsV1().StatefulSets(MCO_NAMESPACE).Get(name, metav1.GetOptions{})
