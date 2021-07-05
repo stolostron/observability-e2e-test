367ad2bc58fa6dfdc4a44c9182bf1156539b61c0
hchenxa <huichen@redhat.com>
2021-07-05 17:55:05 +0800

    need to update the revert test as well
    Signed-off-by: hchenxa <huichen@redhat.com>

--- pkg/tests/observability_reconcile_test.go
+++ pkg/tests/observability_reconcile_test.go
@@ -142,9 +142,17 @@ var _ = Describe("Observability:", func() {
 	})
 
 	It("[P2][Sev2][Observability][Stable] Revert MCO CR changes (reconcile/g0)", func() {
-
+		mcoCR, err := dynClient.Resource(utils.NewMCOGVRV1BETA2()).Get(MCO_CR_NAME, metav1.GetOptions{})
+		Expect(err).NotTo(HaveOccurred())
+		if _, adv := mcoCR.Object["spec"].(map[string]interface{})["advanced"]; !adv {
+			Skip("Skip the case since the MCO CR did not have advanced spec configed")
+		} else {
+			if _, rec := mcoCR.Object["spec"].(map[string]interface{})["advanced"].(map[string]interface{})["retentionConfig"]; !rec {
+				Skip("Skip the case since the MCO CR did not have advanced retentionConfig configed")
+			}
+		}
 		By("Revert MCO CR changes")
-		err := utils.RevertMCOCRModification(testOptions)
+		err = utils.RevertMCOCRModification(testOptions)
 		Expect(err).ToNot(HaveOccurred())
 
 		By("Waiting for MCO retentionResolutionRaw filed to take effect")
