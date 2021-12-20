f523e8a330227ec4405521eb0dd49bf9bc041840
Morven Cao <lcao@redhat.com>
2021-12-20 15:04:29 +0800

    add alert-forward case. (#244)
    * add alert-forward case.
    
    Signed-off-by: morvencao <lcao@redhat.com>
    
    * fix cluster name getter.
    
    Signed-off-by: morvencao <lcao@redhat.com>

--- pkg/utils/mco_managedcluster.go
+++ pkg/utils/mco_managedcluster.go
@@ -3,7 +3,14 @@
 
 package utils
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	"encoding/json"
+	"strings"
+
+	goversion "github.com/hashicorp/go-version"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog"
+)
 
 func UpdateObservabilityFromManagedCluster(opt TestOptions, enableObservability bool) error {
 	clusterName := GetManagedClusterName(opt)
@@ -30,4 +37,67 @@ func UpdateObservabilityFromManagedCluster(opt TestOptions, enableObservability 
 		}
 	}
 	return nil
+}
+
+func ListOCPManagedClusterIDs(opt TestOptions, minVersionStr string) ([]string, error) {
+	minVersion, err := goversion.NewVersion(minVersionStr)
+	if err != nil {
+		return nil, err
+	}
+	clientDynamic := GetKubeClientDynamic(opt, true)
+	objs, err := clientDynamic.Resource(NewOCMManagedClustersGVR()).List(metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	klog.V(3).Infof("objs is %s", objs)
+	clusterIDs := []string{}
+	for _, obj := range objs.Items {
+		metadata := obj.Object["metadata"].(map[string]interface{})
+		klog.V(3).Infof("metadata is %s", metadata)
+		labels := metadata["labels"].(map[string]interface{})
+		klog.V(3).Infof("labels is %s", labels)
+		if labels != nil {
+			vendorStr := ""
+			if vendor, ok := labels["vendor"]; ok {
+				vendorStr = vendor.(string)
+			}
+
+			clusterNameStr := obj.GetName()
+			klog.V(3).Infof("start to get obaAddon")
+			addon, err := clientDynamic.Resource(NewMCOAddonGVR()).Namespace(clusterNameStr).Get("observability-addon", metav1.GetOptions{})
+			klog.V(3).Infof("addon is %s ", addon)
+			if err != nil {
+				return nil, err
+			}
+
+			status, _ := json.MarshalIndent(addon.Object["status"], "", "  ")
+			klog.V(3).Infof("status is %s ", status)
+			obsAddonStatusStr := ""
+			if strings.Contains(string(status), "Cluster metrics sent successfully") {
+				obsAddonStatusStr = "available"
+			}
+
+			if vendorStr == "OpenShift" && obsAddonStatusStr == "available" {
+				clusterVersionStr := ""
+				if clusterVersionVal, ok := labels["openshiftVersion"]; ok {
+					clusterVersionStr = clusterVersionVal.(string)
+				}
+				clusterVersion, err := goversion.NewVersion(clusterVersionStr)
+				if err != nil {
+					return nil, err
+				}
+				if clusterVersion.GreaterThanOrEqual(minVersion) {
+					clusterIDStr := ""
+					if clusterID, ok := labels["clusterID"]; ok {
+						clusterIDStr = clusterID.(string)
+					}
+					if len(clusterIDStr) > 0 {
+						clusterIDs = append(clusterIDs, clusterIDStr)
+					}
+				}
+			}
+		}
+	}
+
+	return clusterIDs, nil
 }
